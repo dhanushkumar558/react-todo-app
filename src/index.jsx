4565f184325dea3a11467af19e1a32e3bd06f1f3
dhanushkumar558 <dhanushkumar558@hotmail.com>
2024-06-08 12:24:19 +0530

    done

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+
+import App from "./App.jsx";
+import "./assets/App.css";
+
+
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import ReactDom from "react-dom"
+
+ReactDom.render(<App />,document.getElementById("root"))
